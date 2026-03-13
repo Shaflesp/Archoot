7f7edc502bbcdf581503a679c28d37a7c9bdf773
loise.vigneron.etu <loise.vigneron.etu@univ-lille.fr>
2026-03-13 11:35:22 +0100

    mise en place logo sur accueil

---
+++ client/src/screens/CreditsView.ts
@@ -0,0 +1,17 @@
+import type { ViewManager } from '../ViewManager.ts';
+import type { View } from './View.ts';
+
+export class GameView implements View {
+    element = document.getElementById('credits-screen')!;
+
+    constructor(sm: ViewManager) {}
+
+    show(): void {
+        console.log('CreditsScreen appelé');
+        this.element.style.display = 'flex';
+    }
+
+    hide(): void {
+        this.element.style.display = 'none';
+    }
+}
