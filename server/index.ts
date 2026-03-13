095de19dbadd2aac2bfa788c078bcbd6e8e18702
Liam Lecnik <liam.lecnik.etu@univ-lille.fr>
2026-03-13 11:15:21 +0100

    Détection des keypress sur le serveur

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,17 @@
-console.log(`Le serveur fonctionne.`);
+import http from 'http';
+import { Server as IOServer } from 'socket.io';
+
+const httpServer = http.createServer((_req, res) => {
+	res.statusCode = 200;
+});
+
+const port = 8080;
+httpServer.listen(port, () => {
+	console.log(`Server is running at http://localhost:${port}.`);
+});
+
+const io = new IOServer(httpServer, { cors: { origin: true } });
+
+io.on('connection', socket => {
+	socket.on('keypress', event => console.log(event));
+});
