ae41456574ec445bfb296793875a68f38a3a5cd5
Liam <liam.lecnik.etu@univ-lille.fr>
2026-03-14 15:06:57 +0100

    Ajout joueur

--- client/src/screens/GameView.ts
+++ client/src/screens/GameView.ts
@@ -29,9 +29,46 @@ export class GameView implements View {
 		socket.on('playerInfo', info => {
 			this.playerInfo = info;
 		});
+
+		window.addEventListener('keydown', e => {
+			switch (e.key) {
+				case 'ArrowUp':
+					socket.emit('keypress', 'up');
+					break;
+				case 'ArrowDown':
+					socket.emit('keypress', 'down');
+					break;
+				case 'ArrowLeft':
+					socket.emit('keypress', 'left');
+					break;
+				case 'ArrowRight':
+					socket.emit('keypress', 'right');
+					break;
+			}
+		});
+
+		this.gameLoop();
 	}
 
 	hide(): void {
 		this.element.style.display = 'none';
+	}
+
+	private gameLoop = () => {
+		this.draw();
+		requestAnimationFrame(this.gameLoop);
+	};
+
+	private draw() {
+		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+		for (const identifier in this.playerInfo) {
+			const p = this.playerInfo[identifier];
+
+			const image = new Image();
+			image.src = '/images/cobaye.png';
+
+			this.ctx.drawImage(image, p.x, p.y, 30, 30);
+		}
 	}
 }
